cddcfe260e929db206af374f207a58c50873d2f7
Thomas Kirkwood <Tkirkwood@spartaglobal.com>
2023-03-28 12:03:10 +0100

    fixed spelling mistake in reset it here link aria-label

--- dte/src/components/Shared/Login/UserLogin.tsx
+++ dte/src/components/Shared/Login/UserLogin.tsx
@@ -290,7 +290,7 @@ const UserLogin = () => {
                     <DTERouteLink
                       to="/ForgottenPassword"
                       renderStyle="standard"
-                      ariaLabel="reset you password here"
+                      ariaLabel="reset your password here"
                     >
                       reset it here.
                     </DTERouteLink>
