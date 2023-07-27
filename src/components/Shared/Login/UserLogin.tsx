b499532d9514e9a4136d2afde9bd757936a7e392
Paddy Duff <paddy.duff31@googlemail.com>
2023-07-27 13:34:22 +0100

    Moved the nhs login error message

--- src/components/Shared/Login/UserLogin.tsx
+++ src/components/Shared/Login/UserLogin.tsx
@@ -183,7 +183,7 @@ const UserLogin = (props: UserLoginProps) => {
             detail = `Enter the email address and password for a registered user account.${
               !nested
                 ? " If you registered using NHS login use the back button above and select NHS login to sign in."
-                : " If you registered using NHS login use the back button above and select NHS login visit the sign in page and select NHS login to sign in."
+                : ""
             }`;
           } else if (e.customCode === "Mfa_Setup_Challenge") {
             detail =
@@ -297,6 +297,17 @@ const UserLogin = (props: UserLoginProps) => {
               >
                 reset it here.
               </DTERouteLink>
+            </DTEContent>
+            <DTEContent>
+              If you registered using NHS login,{" "}
+              <DTERouteLink
+                to="/Participants/Options"
+                renderStyle="standard"
+                ariaLabel="visit the sign in options page"
+              >
+                visit the sign in options page
+              </DTERouteLink>{" "}
+              and select NHS login to sign in.
             </DTEContent>
             <ButtonWrapper>
               <DTEButton disabled={loadingLogin}>Sign in</DTEButton>
