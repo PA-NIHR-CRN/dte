4eb89201f1da574b8dba07b624ab9345786e1b1b
Paddy Duff <paddy.duff31@googlemail.com>
2023-08-22 11:18:11 +0100

    added UserProvider context to test-utils

--- src/Helper/test-utils.tsx
+++ src/Helper/test-utils.tsx
@@ -11,6 +11,7 @@ import { AuthProvider } from "../context/AuthContext";
 import ManualLoginProvider from "../context/ManualLoginContext";
 import { ContentContext } from "../context/ContentContext";
 import { getContent } from "./contenful/contentHandler";
+import { UserProvider } from "../context/UserContext";
 
 interface RenderProps {
   children?: any;
@@ -34,46 +35,48 @@ function AllTheProviders({ children, initialRoutes }: RenderProps) {
   const MuiTheme = useTheme();
   return (
     <AuthProvider>
-      <ContentContext.Provider
-        value={{
-          content: mockContent,
-          setLanguage: () => {},
-          contentLoading: false,
-          language: "en-GB",
-        }}
-      >
-        <StylesProvider injectFirst>
-          <ThemeProvider theme={{ ...styledComponentsTheme, ...MuiTheme }}>
-            <SnackbarProvider
-              maxSnack={3}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              // TransitionComponent={Slide}
-            >
-              <AppRoot />
-              <AppContext.Provider
-                value={{
-                  lang: "de",
-                  theme: Theme.Light,
-                  setTheme: () => {},
-                  showHeader: true,
-                  setShowHeader: () => {},
-                  showBacklink: false,
-                  setShowBacklink: () => {},
-                  showSplotches: false,
-                  setShowSplotches: () => {},
+      <UserProvider>
+        <ContentContext.Provider
+          value={{
+            content: mockContent,
+            setLanguage: () => {},
+            contentLoading: false,
+            language: "en-GB",
+          }}
+        >
+          <StylesProvider injectFirst>
+            <ThemeProvider theme={{ ...styledComponentsTheme, ...MuiTheme }}>
+              <SnackbarProvider
+                maxSnack={3}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
                 }}
+                // TransitionComponent={Slide}
               >
-                <MemoryRouter initialEntries={initialRoutes ?? ["/"]}>
-                  <ManualLoginProvider>{children}</ManualLoginProvider>
-                </MemoryRouter>
-              </AppContext.Provider>
-            </SnackbarProvider>
-          </ThemeProvider>
-        </StylesProvider>
-      </ContentContext.Provider>
+                <AppRoot />
+                <AppContext.Provider
+                  value={{
+                    lang: "de",
+                    theme: Theme.Light,
+                    setTheme: () => {},
+                    showHeader: true,
+                    setShowHeader: () => {},
+                    showBacklink: false,
+                    setShowBacklink: () => {},
+                    showSplotches: false,
+                    setShowSplotches: () => {},
+                  }}
+                >
+                  <MemoryRouter initialEntries={initialRoutes ?? ["/"]}>
+                    <ManualLoginProvider>{children}</ManualLoginProvider>
+                  </MemoryRouter>
+                </AppContext.Provider>
+              </SnackbarProvider>
+            </ThemeProvider>
+          </StylesProvider>
+        </ContentContext.Provider>
+      </UserProvider>
     </AuthProvider>
   );
 }
