4b816b1a8691f962ae33b15e97c22a02d30cce48
dcnihr <dale.christian@nihr.ac.uk>
2023-09-05 10:41:54 +0100

    health conditions

--- src/components/Shared/FormElements/HealthConditionForm.tsx
+++ src/components/Shared/FormElements/HealthConditionForm.tsx
@@ -3,9 +3,6 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useTheme } from "@material-ui/core/styles";
 import { Controller, useForm } from "react-hook-form";
 import DTEHeader from "../UI/DTETypography/DTEHeader/DTEHeader";
-import DTEContent from "../UI/DTETypography/DTEContent/DTEContent";
-import DTERouteLink from "../UI/DTERouteLink/DTERouteLink";
-import DTEDetails from "../UI/DTEDetails/DTEDetails";
 import FormBaseProps from "./FormBaseProps";
 import DTEForwardLookup from "../UI/DTEForwardLookup/DTEForwardLookup";
 import FormNavigationButtons from "./CommonElements/FormNavigationButtons";
@@ -49,29 +46,10 @@ function HealthConditionForm(props: HealthConditionFormProps) {
     <>
       {!hideHeader && (
         <DTEHeader as="h1" $variant={headerVariant}>
-          Which areas of research are you interested in?
+          {content["register2-health-conditions-header"]}
         </DTEHeader>
       )}
-      {instructionText || (
-        <>
-          <DTEContent>
-            Start entering details below to see different areas of research. You can select as many options as you like.
-          </DTEContent>
-          <DTEContent>
-            For example, you can enter a health condition like &apos;diabetes&apos;, &apos;heart disease&apos; or
-            &apos;COVID-19&apos;. You can take part whether you have a health condition or not by entering &apos;
-            <DTERouteLink
-              to="https://bepartofresearch.nihr.ac.uk/taking-part/volunteering-without-a-condition/"
-              external
-              target="_blank"
-              renderStyle="standard"
-            >
-              healthy volunteers
-            </DTERouteLink>
-            &apos;.
-          </DTEContent>
-        </>
-      )}
+      {instructionText || content["register2-health-conditions-instruction-text"]}
       <Grid container>
         <Grid item xs={12} sm={10} md={9} lg={8} xl={8}>
           <form onSubmit={handleSubmit(onDataChange)} noValidate>
@@ -89,20 +67,7 @@ function HealthConditionForm(props: HealthConditionFormProps) {
                 />
               )}
             />
-            {!hideInfo && (
-              <DTEDetails summary="Why we are asking this question">
-                <>
-                  <DTEContent>
-                    We will use this information to contact you about studies you may be interested in based on the
-                    areas of research you select here.
-                  </DTEContent>
-                  <DTEContent>
-                    If you do not select any areas of research we will only be able to let you know about a limited
-                    number of studies based on the other information you have given during your account creation.
-                  </DTEContent>
-                </>
-              </DTEDetails>
-            )}
+            {!hideInfo && content["register2-health-conditions"]}
             <FormNavigationButtons
               nextButtonText={nextButtonText || content["reusable-button-continue"]}
               showCancelButton={showCancelButton || false}
