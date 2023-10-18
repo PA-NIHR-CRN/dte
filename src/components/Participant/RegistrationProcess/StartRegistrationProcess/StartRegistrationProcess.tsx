43d53f60486a20f811cad702310752d5ba9e5b6b
RahatH7 <147616337+RahatH07@users.noreply.github.com>
2023-10-18 10:15:54 +0100

    Just missing NHS button funcionality
    added, register with email button, got rid of sign up button, changed to anchor tag, added a back text, that is based of users history

--- src/components/Participant/RegistrationProcess/StartRegistrationProcess/StartRegistrationProcess.tsx
+++ src/components/Participant/RegistrationProcess/StartRegistrationProcess/StartRegistrationProcess.tsx
@@ -2,13 +2,41 @@ import DocumentTitle from "react-document-title";
 import StepWrapper from "../../../Shared/StepWrapper/StepWrapper";
 import { ContentContext } from "../../../../context/ContentContext";
 import { useContext } from "react";
+import { Stack } from "@mui/material";
+import DTEButton from "../../../Shared/UI/DTEButton/DTEButton";
+import { useHistory } from "react-router-dom";
 
 function StartRegistrationProcess() {
   const { content } = useContext(ContentContext);
+  const history = useHistory();
+
+  const handleGoBack = () => {
+    history.goBack();
+  };
 
   return (
     <DocumentTitle title={content["register-page-document-title"]}>
-      <StepWrapper>{content["register-page"]}</StepWrapper>
+      <StepWrapper>
+        {content["register-page"]}
+        <Stack spacing={2} direction="row">
+          <a href="https://access.login.nhs.uk/enter-email">
+            <DTEButton $outlined>Continue to NHS login</DTEButton>
+          </a>
+          <a href="https://volunteer.bepartofresearch.nihr.ac.uk/Participants/Register/Questions">
+            <DTEButton $padded>Register with email address</DTEButton>
+          </a>
+        </Stack>
+        <br></br>
+        <span style={{ margin: "10px 0" }}>
+          If you already have an account, you can{" "}
+          <a href="https://volunteer.bepartofresearch.nihr.ac.uk/Participants/Options">sign in</a>.
+        </span>
+        <br></br>
+        <br></br>
+        <a href="#" onClick={handleGoBack}>
+          Back
+        </a>
+      </StepWrapper>
     </DocumentTitle>
   );
 }
