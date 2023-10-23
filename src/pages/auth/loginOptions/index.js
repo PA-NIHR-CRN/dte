47ef0c25022e5d3464f5774e8ea9f76feaf23bb9
RahatH7 <147616337+RahatH07@users.noreply.github.com>
2023-10-23 11:02:47 +0100

    All changed added 1630
    It was easier the second time :)

--- src/pages/auth/loginOptions/index.js
+++ src/pages/auth/loginOptions/index.js
@@ -6,6 +6,10 @@ import DocumentTitle from "react-document-title";
 import { AuthContext } from "../../../context/AuthContext";
 import { ContentContext } from "../../../context/ContentContext";
 import DTEHeader from "../../../components/Shared/UI/DTETypography/DTEHeader/DTEHeader";
+import NhsLoginButton from "../../../components/Shared/UI/NhsLoginButton";
+import DTEButton from "../../../components/Shared/UI/DTEButton/DTEButton";
+import DTEContent from "../../../components/Shared/UI/DTETypography/DTEContent/DTEContent";
+import DTELinkButton from "../../../components/Shared/UI/DTELinkButton/DTELinkButton";
 
 export const LoginWrapper = styled(Grid)`
   margin: 40px 10px;
@@ -16,6 +20,10 @@ function LoginOptions() {
   const history = useHistory();
   const { content } = useContext(ContentContext);
 
+  const handleGoBack = () => {
+    history.goBack();
+  };
+
   if (isAuthenticated()) {
     history.push("/");
   }
@@ -24,7 +32,27 @@ function LoginOptions() {
       <Grid container justifyContent="center" alignItems="center" role="main" id="main">
         <LoginWrapper item xs={12} sm={8} md={6} lg={5} xl={4}>
           <DTEHeader as="h1">{content["signin-options-header"]}</DTEHeader>
+          <a href="#" onClick={handleGoBack}>
+            {"< "} Back
+          </a>
           {content["signin-options"]}
+          <div style={{ display: "flex", gap: "2rem", alignItems: "center", justifyContent: "flex-start" }}>
+            <div style={{ marginLeft: "5px" }}>
+              <NhsLoginButton buttonText="Continue to NHS login" />
+            </div>
+            <div style={{ marginTop: "39.75px" }}>
+              <DTEButton $outlined onClick={() => history.push("/Userlogin")}>
+                Sign in with email address
+              </DTEButton>
+            </div>
+          </div>
+          <br></br>
+          <DTEContent>
+            Need an account?{" "}
+            <DTELinkButton onClick={() => history.push("/Participants/register")}>Register here.</DTELinkButton>
+          </DTEContent>
+          <br></br>
+          <br></br>
         </LoginWrapper>
       </Grid>
     </DocumentTitle>
