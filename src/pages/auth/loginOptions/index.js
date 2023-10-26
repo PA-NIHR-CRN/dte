62cb5688ebb632cba5787051a6f71c4b46027f93
Thomas Smolen <thomas.smolen@paconsulting.com>
2023-10-26 16:51:46 +0100

    CRNCC-1630
    Added Backlink at the top of the page.
    Added NHS login button and outlined sign in button side by side.
    Added text with "register here' text button to take user to register page

--- src/pages/auth/loginOptions/index.js
+++ src/pages/auth/loginOptions/index.js
@@ -1,56 +1,69 @@
 import { useHistory } from "react-router-dom";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Grid } from "@material-ui/core";
 import styled from "styled-components";
 import DocumentTitle from "react-document-title";
-import { AuthContext } from "../../../context/AuthContext";
-import { ContentContext } from "../../../context/ContentContext";
+import DTERouteLink from "../../../components/Shared/UI/DTERouteLink/DTERouteLink";
 import DTEHeader from "../../../components/Shared/UI/DTETypography/DTEHeader/DTEHeader";
+import DTEContent from "../../../components/Shared/UI/DTETypography/DTEContent/DTEContent";
+import NhsLoginButton from "../../../components/Shared/UI/NhsLoginButton";
+import { AuthContext } from "../../../context/AuthContext";
 import DTEBackLink from "../../../components/Shared/UI/DTEBackLink/DTEBackLink";
 
 export const LoginWrapper = styled(Grid)`
-  margin: 20px 10px 40px 10px;
+  margin: 40px 10px;
 `;
 
-const StyledGridElementLeft = styled(Grid)`
-  padding-left: 1em;
-  && {
-    text-align: left;
-  }
-  padding-bottom: -1em;
-  margin-bottom: 0;
+const ButtonWrapper = styled.div`
+  margin-top: 1rem;
 `;
 
-function LoginOptions() {
+const ButtonContainer = styled.div`
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+`;
+
+const LoginOptions = () => {
   const { isAuthenticated } = useContext(AuthContext);
   const history = useHistory();
-  const { content } = useContext(ContentContext);
 
   if (isAuthenticated()) {
     history.push("/");
   }
   return (
-    <DocumentTitle title={content["signin-options-document-title"]}>
+    <DocumentTitle title="Sign in or register - Volunteer Registration - Be Part of Research">
       <>
-        <Grid container alignItems="center" justifyContent="flex-start">
-          <Grid item sm={2} md={1} />
-          <StyledGridElementLeft item xs={12} sm={10} md={11}>
-            <DTEBackLink
-              linkText={content["reusable-back-link"]}
-              onClick={() => history.goBack()}
-              ariaLabel={content["reusable-aria-go-back"]}
-            />
-          </StyledGridElementLeft>
-        </Grid>
-        <Grid container justifyContent="center" alignItems="center" role="main" id="main">
+        <Grid container justifyContent="center" alignItems="center" role="main">
+          <DTEBackLink linkText="Back" onClick={() => history.goBack()} />
           <LoginWrapper item xs={12} sm={8} md={6} lg={5} xl={4}>
-            <DTEHeader as="h1">{content["signin-options-header"]}</DTEHeader>
-            {content["signin-options"]}
+            <DTEHeader as="h1">Sign in to Be Part of Research</DTEHeader>
+            <ButtonContainer>
+              <Grid>
+                <NhsLoginButton />
+                <ButtonWrapper>
+                  <DTERouteLink to="/UserLogin" $outlined={true}>
+                    Sign in with email address
+                  </DTERouteLink>
+                </ButtonWrapper>
+              </Grid>
+            </ButtonContainer>
+
+            <DTEContent
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              Need an account?
+              <DTELinkButton customStyles={{ marginLeft: "5px" }} onClick={() => history.push("/Participants/Options")}>
+                Register here
+              </DTELinkButton>
+            </DTEContent>
           </LoginWrapper>
         </Grid>
       </>
     </DocumentTitle>
   );
-}
+};
 
 export default LoginOptions;
